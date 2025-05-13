2ad94278ab2909dbe5bea4b79f2060d5cd3ef737
hautr33 <hautt33@gmail.com>
2025-05-13 10:58:07 +0700

    update interface IBookGetter, IBookSetter and intersection type IBook

--- Day05_Typescript/src/book.ts
+++ Day05_Typescript/src/book.ts
@@ -1,73 +1,100 @@
 import { BookStatus } from "./util";
 
-interface IBook {
-    id: number | null;
+interface IBookGetter {
+    getId(): number | null;
+
+    getTitle(): string;
+
+    getAuthor(): string;
+
+    getYear(): number;
+
+    getGenre(): string;
+
+    getStatus(): string;
 }
 
+interface IBookSetter {
+    setId(id: number): void
+
+    setTitle(title: string): void
+
+    setAuthor(author: string): void
+
+    setYear(year: number): void
+
+    setGenre(genre: string): void
+
+    setStatus(status: 'Available' | 'Checked Out' | 'Lost'): void
+}
+
+type IBook = IBookGetter & IBookSetter
+
+
 export class Book implements IBook {
-    id: number | null =null;
-    private title: string;
-    private author: string;
-    private year: number;
-    private genre: string;
-    private status: BookStatus = 'Available';
+    private _id: number | null = null;
+    private _title: string;
+    private _author: string;
+    private _year: number;
+    private _genre: string;
+    private _status: BookStatus = 'Available';
 
     constructor(title: string, author: string, year: number, genre: string) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-        this.genre = genre;
+        this._title = title;
+        this._author = author;
+        this._year = year;
+        this._genre = genre;
     }
 
     getId(): number | null {
-        return this.id;
+        return this._id;
     }
 
     getTitle(): string {
-        return this.title;
+        return this._title;
     }
 
     getAuthor(): string {
-        return this.author;
+        return this._author;
     }
 
     getYear(): number {
-        return this.year;
+        return this._year;
     }
 
     getGenre(): string {
-        return this.genre;
+        return this._genre;
     }
 
     getStatus(): string {
-        return this.status;
+        return this._status;
     }
 
     setId(id: number): void {
-        this.id = id;
+        this._id = id;
     }
 
     setTitle(title: string): void {
-        this.title = title;
+        this._title = title;
     }
 
     setAuthor(author: string): void {
-        this.author = author;
+        this._author = author;
     }
 
     setYear(year: number): void {
-        this.year = year;
+        this._year = year;
     }
 
     setGenre(genre: string): void {
-        this.genre = genre;
+        this._genre = genre;
     }
 
     setStatus(status: 'Available' | 'Checked Out' | 'Lost'): void {
-        this.status = status;
+        this._status = status;
     }
 
     toString(): string {
-        return `${this.id!.toString().padEnd(3, ' ')} | ${this.title.padEnd(50, ' ')} | ${this.author.padEnd(30, ' ')} | ${this.year.toString().padEnd(4, ' ')} | ${this.genre.padEnd(25, ' ')} | ${this.status.padEnd(11, ' ')}`;
+        return `${(this._id as number).toString().padEnd(3, ' ')} | ${this._title.padEnd(50, ' ')} | ${this._author.padEnd(30, ' ')} | ${this._year.toString().padEnd(4, ' ')} | ${this._genre.padEnd(25, ' ')} | ${this._status.padEnd(11, ' ')}`;
     }
 }
